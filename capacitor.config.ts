aca12d89477d130044b2aef5ddb5fa10a66ae544
Matur Innocent <maturinnocent@gmail.com>
2025-07-03 17:09:29 +0100

    first push

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.delivery',
+  appName: 'delivery',
+  webDir: 'dist'
+};
+
+export default config;
